f6eed16d2538a5608e853937c5e2c76e08dbfe26
Juan villarroel <vilfrack@users.noreply.github.com>
2018-10-26 17:29:46 -0400

    Se realiza el insert y mostrar los datos

---
+++ WebApi/WebApplication1/Controllers/EmployeeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+using System.Net.Http;
+
+namespace WebApplication1.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            /*EN GlobalVariables SE OBTENDAN LOS CODIGOS PARA CONECTARSE A LA API*/
+            IEnumerable<mvcEmployeeModel> empList;
+            HttpResponseMessage response = GlobalVariables.WebApiClient.GetAsync("Employee").Result;
+            //se guarda en la lista todos los datos obtenidos de la api
+            empList = response.Content.ReadAsAsync<IEnumerable<mvcEmployeeModel>>().Result;
+            return View(empList);
+        }
+        public ActionResult AddOrEdit() {
+            return View(new mvcEmployeeModel());
+        }
+        [HttpPost]
+        public ActionResult AddOrEdit(mvcEmployeeModel emp)
+        {
+            HttpResponseMessage responde = GlobalVariables.WebApiClient.PostAsJsonAsync("Employee", emp).Result;
+            TempData["SuccessMessage"] = "Save Successfully";
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ WebApi/WebApplication1/GlobalVariables.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Net.Http.Headers;
+
+namespace WebApplication1
+{
+    public static class GlobalVariables
+    {
+        public static HttpClient WebApiClient = new HttpClient();
+
+        static GlobalVariables() {
+            WebApiClient.BaseAddress = new Uri("http://localhost:60869/api/");
+            WebApiClient.DefaultRequestHeaders.Clear();
+            WebApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+    }
+}
