40263e6f1fd5539acf0c1b91302135bf79940f32
Juan villarroel <vilfrack@users.noreply.github.com>
2018-10-30 22:24:16 -0400

    se crea el crud completo de la api.

--- WebApi/WebApplication1/Controllers/EmployeeController.cs
+++ WebApi/WebApplication1/Controllers/EmployeeController.cs
@@ -20,14 +20,35 @@ namespace WebApplication1.Controllers
             empList = response.Content.ReadAsAsync<IEnumerable<mvcEmployeeModel>>().Result;
             return View(empList);
         }
-        public ActionResult AddOrEdit() {
-            return View(new mvcEmployeeModel());
+        public ActionResult AddOrEdit( int id = 0) {
+            if (id==0)
+                return View(new mvcEmployeeModel());
+            else
+            {
+                HttpResponseMessage responde = GlobalVariables.WebApiClient.GetAsync("Employee/"+id.ToString()).Result;
+                return View(responde.Content.ReadAsAsync<mvcEmployeeModel>().Result);
+            }
         }
         [HttpPost]
         public ActionResult AddOrEdit(mvcEmployeeModel emp)
         {
-            HttpResponseMessage responde = GlobalVariables.WebApiClient.PostAsJsonAsync("Employee", emp).Result;
-            TempData["SuccessMessage"] = "Save Successfully";
+            if (emp.EmployeeID ==0)
+            {
+                HttpResponseMessage responde = GlobalVariables.WebApiClient.PostAsJsonAsync("Employee", emp).Result;
+                TempData["SuccessMessage"] = "Save Successfully";
+            }
+            else
+            {
+                HttpResponseMessage responde = GlobalVariables.WebApiClient.PutAsJsonAsync("Employee/"+emp.EmployeeID, emp).Result;
+                TempData["SuccessMessage"] = "Update Successfully";
+            }
+            return RedirectToAction("Index");
+        }
+
+
+        public ActionResult Delete(int id) {
+            HttpResponseMessage responde = GlobalVariables.WebApiClient.DeleteAsync("Employee/"+ id).Result;
+            TempData["SuccessMessage"] = "Delete Successfully";
             return RedirectToAction("Index");
         }
     }
